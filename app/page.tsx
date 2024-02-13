bf9a21f164401d278a80b288948d2b8a5ece73f1
Cindy Wu <ciindy.wu@gmail.com>
2024-02-12 17:41:46 -1000

    skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import Footer from "./ui/footer";
+
 export default function Page() {
+  const heroText = `Coming May 2024, Pictoplasma celebrates its 20th Berlin Conference and Festival anniversary edition with a special XXL line-up of awe inducing back-to-back conference lectures, freshly curated animation screenings, exhibitions, immersive media installations, networking, art markets, all-night parties, and the very best in contemporary character design! `;
   return (
-    <div className={"text-3xl font-bold underline"}>
-      picto clone
+    <div className={"w-full"}>
+      <div className={"p-2 text-4xl uppercase"}>Pictoplasma</div>
+      <div className={"m-2 h-48 bg-gray-300"}></div>
+      <div className={"p-2"}>{heroText}</div>
+      <div className={"p-2"}>Tickets now available!</div>
+      <div className={"flex justify-end p-2"}>
+        <button className={"border border-black p-2 uppercase"}>
+          > Picto Conference
+        </button>
+      </div>
+      <div className={"m-2 h-48 bg-gray-300"}></div>
       <Footer />
     </div>
   );
--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,3 +1,37 @@
 export default function Footer() {
-  return <div>footer</div>;
+  const newsletterDescriptionText = `Picto news are good news! Sign up for our semi-monthly newsletter - and we'll keep you up to date on all our upcoming fesitvals, conferences, exhibitions, publications, submission deadlines, call for entries - and all things character design and art!`;
+  return (
+    <div className={"flex w-full flex-col text-center"}>
+      <div className={"flex flex-row justify-center"}>
+        <div className={"w-96 pb-8"}>
+          <div className={"text-2xl uppercase"}>Pictoplasma</div>
+          <div className={"uppercase"}>
+            Off- and online community for characters & creators
+          </div>
+        </div>
+      </div>
+      <div className={"flex flex-row justify-center pb-10"}>
+        <div className={"w-96"}>
+          <div className={"p-2"}>Sign up for picto news</div>
+          <div className={"text-xs"}>{newsletterDescriptionText}</div>
+          <div className={"p-2"}>
+            <input
+              className={"w-96 border border-black p-2 p-2 text-xs"}
+              placeholder={"enter your email address"}
+            />
+          </div>
+          <div className={"py-2 text-xs"}>
+            <input className={"m-2"} type="checkbox" />
+            <label>
+              I allow Pictoplasma to send me newsletters -- and know I can
+              un-subscribe at any time
+            </label>
+          </div>
+          <button className={"border border-black px-2 py-1 uppercase"}>
+            Sign up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
